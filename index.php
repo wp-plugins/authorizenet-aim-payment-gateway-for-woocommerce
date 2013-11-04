a9c07ca363ca019250f5b589990de2785c838ac1
ishanverma <ishanverma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-04 09:15:22 +0000

    bug fix 1.2.4
    git-svn-id: https://plugins.svn.wordpress.org/authorizenet-aim-payment-gateway-for-woocommerce/trunk@798479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Authorize.net AIM Payment Gateway For WooCommerce
 Description: Extends WooCommerce to Process Payments with Authorize.net AIM gateway
-Version: 1.2.3
+Version: 1.2.4
 Plugin URI: http://www.phptubelight.com?source=autho-aim
 Author: Ishan Verma
 Author URI: http://www.phptubelight.com?source=autho-aim
